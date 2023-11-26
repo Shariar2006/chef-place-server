4cf433208aca2dae69deb0a749c5db20466c86ef
Shariar <shariarnafis86@gmail.com>
2023-11-26 17:06:48 +0600

    get meal details

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 //middle ware 
@@ -16,35 +16,42 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    //DB collection
-    const menuCollection = client.db('chef-place').collection('foodMenu')
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //DB collection
+        const menuCollection = client.db('chef-place').collection('allMeals')
 
 
-    //all meals
-    app.get('/allMeals', async(req, res)=>{
-        const result = await menuCollection.find().toArray()
-        res.send(result)
-    })  
+        //all meals
+        app.get('/allMeals', async (req, res) => {
+            const result = await menuCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/meal/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.findOne(query)
+            res.send(result)
+        })
 
 
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -52,8 +59,8 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('chef place server is running')
 })
 
-app.listen(port,()=>{})
+app.listen(port, () => { })
